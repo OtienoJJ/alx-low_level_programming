871e7e5d1657619db33418e451fd61b290b240f0
OtienoJJ <jumaj17@gmail.com>
2022-10-20 07:45:26 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * largest_number - returns the largest of numbers
+ * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
