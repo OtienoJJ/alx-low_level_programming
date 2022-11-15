53d14c64d2ef522bfcb08692afc89df25d952e50
OtienoJJ <jumaj17@gmail.com>
2022-11-15 11:58:52 -0800

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,9 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - typedef for struct dog
+ */
 
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -21,5 +24,5 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t, *d);
 
-#endif /*DOG_H*/
+#endif
 
